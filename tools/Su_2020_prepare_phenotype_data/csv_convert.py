f8d0b1ce3314efaef7c143ee527074e319a7f3fd
jundahuang9123 <junda.huang9123@gmail.com>
2022-11-14 12:10:16 +0100

    test data for new scripts

--- tools/Su_2020_prepare_phenotype_data/csv_convert.py
+++ tools/Su_2020_prepare_phenotype_data/csv_convert.py
@@ -14,9 +14,17 @@ def csv_parse(csv_file):
     return df
 
 def csv_convert(df):
-    # convert csv file to numpy array
-    # get column names
+    '''
+    convert csv file to numpy array
+    params: pandas dataframe of the input csv file
+    return: dictionaries of numpy arrays
+    '''
+
+    # get the column names, skip first row as they are ontoloy terms
+    df.columns = df.iloc[0]
+    df = df.iloc[1:].reset_index(drop=True)
     col_names = df.columns
+    
     
     # get number of columns
     num_cols = len(col_names)
@@ -24,7 +32,7 @@ def csv_convert(df):
     # get number of rows
     num_rows = len(df.index)
     
-        # create numpy array
+    # create numpy array
     np_array = np.zeros((num_rows, num_cols))
     
     # fill numpy array
@@ -40,8 +48,10 @@ def csv_save(np_array, csv_file):
 
 # main
 if __name__ == "__main__":
+
+    file = 'C:\Users\z370170\Projects\TWOC\TWOCdemonstrator\tool\test_data\TWOC-phenotypes.csv'
     # read in csv file
-    df = csv_parse('')
+    df = csv_parse(file)
     
     # convert csv file to numpy array
     np_array = csv_convert(df)
