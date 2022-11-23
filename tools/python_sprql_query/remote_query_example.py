c62d59e6ac0ff569a1b282812540e0848b349eb2
jundahuang9123 <junda.huang9123@gmail.com>
2022-11-23 14:53:32 +0100

    doctring added

--- tools/python_sprql_query/remote_query_example.py
+++ tools/python_sprql_query/remote_query_example.py
@@ -3,10 +3,21 @@ from rdflib import Graph, URIRef
 # functions
 
 def sparql_query_to_file(q, filename):
+    """
+    function to query a sparql endpoint and save the results to a file
+
+    params:
+        q(str): sparql query
+            The endpoint needs to be specified in the query 
+            with SERVICE <endpoint> { ... }
+        filename(str): name of the file to save the results to
+    """
     fdp_g = Graph()
     with open(filename, 'w') as f:
         for s, p, o in fdp_g.query(q):
             f.write(s.n3() + ' ' + p.n3() + ' ' + o.n3() + ' .\n')
+            #s, p, o are in URIRef format needs to be converted to string 
+            # using n3() method
     return None
 
 if __name__ == '__main__':
@@ -27,11 +38,15 @@ if __name__ == '__main__':
                 ?s mondo:C173069 ?o .}
                 FILTER (?o = true)
             }"""
+    # q1 can be queried but since the result automatically gets 
+    # converted to strings and saved to a file, errors occurs here
 
     q2 = """
         select *
             where { SERVICE <https://fdp.cmbi.umcn.nl/blazegraph/sparql>{
                 ?s ?p ?o .}
             } LIMIT 1000 """  
+    # q2 is used to generte a file with 1000 triples of all the data in the
+    # fdp triplestore
     fname = 'test_result.txt'
     sparql_query_to_file(q2, fname)
