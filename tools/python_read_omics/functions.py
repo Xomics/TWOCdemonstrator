a806b0b4f3402f36f726cd45f2d1a88683b6ef49
niehues <1405356+niehues@users.noreply.github.com>
2022-11-23 10:08:00 +0000

    python script with basic functions for reading omics data and feature metadata

---
+++ tools/python_read_omics/functions.py
@@ -0,0 +1,11 @@
+# functions to read omics data and omics feature metadata from tabular files
+
+# function to read omics file into pandas data frame
+
+# function to read omics feature metadata file into pandas data frame
+
+# function to create PID from available omics feature metadata
+
+# subset based on sample IDs / feature IDs
+
+# calculate mean
