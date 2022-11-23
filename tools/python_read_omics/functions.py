21eb6aedee06e4bc80309cb48853a94ce803da84
niehues <1405356+niehues@users.noreply.github.com>
2022-11-23 11:35:30 +0100

    started adding first function, needs testing

--- tools/python_read_omics/functions.py
+++ tools/python_read_omics/functions.py
@@ -1,6 +1,12 @@
 # functions to read omics data and omics feature metadata from tabular files
 
+import pandas as pd
+
 # function to read omics file into pandas data frame
+# input:
+def read_omics_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
 
 # function to read omics feature metadata file into pandas data frame
 
@@ -9,3 +15,7 @@
 # subset based on sample IDs / feature IDs
 
 # calculate mean
+
+
+df = read_omics_data("../../data/Su_2020_FAIR/metabolomics/metabolomics_Su_2020_feature-data.csv")
+print(df.shape)
