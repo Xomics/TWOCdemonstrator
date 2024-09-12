b64f94e2d99f4a960628f13475352d3554f18b55
Firdaws <badmusanisat@gmail.com>
2024-09-12 12:08:13 +0200

    added keyword search to workflow

--- tools/python_read_omics/keyword_search.py
+++ tools/python_read_omics/keyword_search.py
@@ -1,6 +1,3 @@
-''' usage: python keyword_search.py "https://sparql.wikipathways.org/sparql" SARS-CoV-2
-'''
-import sys
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def run_keyword_search(endpoint_url, keyword):
@@ -25,14 +22,3 @@ def run_keyword_search(endpoint_url, keyword):
         pathway = result["pathway"]["value"]
         label = result["label"]["value"]
         print(f"Pathway: {pathway}, Label: {label}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <SPARQL_ENDPOINT_URL> <PATHWAY_ID>")
-        sys.exit(1)
-    endpoint_url = sys.argv[1]
-    keyword = sys.argv[2]
-    # endpoint_url = "https://sparql.wikipathways.org/sparql"
-    # keyword = "SARS-CoV-2"  
-
-    run_keyword_search(endpoint_url, keyword)
